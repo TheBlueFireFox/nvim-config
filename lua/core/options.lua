7f205e043158285229fa70fc2d3d86a48f2900bf
Adrian Hornung <adrian.hornung@yahoo.de>
2024-01-14 15:17:24 +0100

    FIX: Treesitter & Clean up old configs

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -25,39 +25,25 @@ vim.g.mapleader = ","
 -- force vim to use a single column for both number and error hightlighting
 vim.opt.signcolumn = "number"
 
--- Disable virtual_text since it's redundant due to lsp_lines.
-vim.diagnostic.config({
-    virtual_text = false,
-})
--- change diagnostics error prefix symbol
-vim.diagnostic.config({
-    virtual_text = {
-        -- prefix = "◯ ",
-    },
-})
-
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
 -- allow for clipboard support on wsl
--- TODO: change to lua
 if require("core.utils").is_wsl then
-    vim.cmd([[
-   " WSL yank support
-    set clipboard+=unnamedplus
-    let g:clipboard = {
-          \   'name': 'win32yank-wsl',
-          \   'copy': {
-          \      '+': 'win32yank.exe -i --crlf',
-          \      '*': 'win32yank.exe -i --crlf',
-          \    },
-          \   'paste': {
-          \      '+': 'win32yank.exe -o --lf',
-          \      '*': 'win32yank.exe -o --lf',
-          \   },
-          \   'cache_enabled': 0,
-          \ }
-   " get browser working
-   let g:netrw_browsex_viewer="cmd.exe /C start"
-   ]])
+    -- WSL yank support
+    vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+    vim.g.clipboard = {
+        name = 'win32yank-wsl',
+        copy = {
+            ['+'] = 'win32yank.exe -i --crlf',
+            ['*'] = 'win32yank.exe -i --crlf',
+        },
+        paste = {
+            ["+"] = 'win32yank.exe -o --lf',
+            ["*"] = 'win32yank.exe -o --lf',
+        },
+        cache_enabled = 0,
+    }
+    -- get browser working
+    vim.g.netrw_browsex_viewer = "cmd.exe /C start"
 end
