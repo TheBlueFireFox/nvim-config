10b2d55dad3012969cf67befeca89c26c59d71f6
Adrian Hornung <adrian.hornung@yahoo.de>
2024-03-19 10:23:12 +0100

    add the copilot integration

--- lua/plugins/useful.lua
+++ lua/plugins/useful.lua
@@ -42,4 +42,15 @@ return {
             },
         },
     },
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+            },
+        }
+    }
 }
