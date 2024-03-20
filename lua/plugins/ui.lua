9cd169162f5f81239008d4267844cd9dbd8667da
Adrian Hornung <adrian.hornung@yahoo.de>
2024-03-20 08:44:28 +0100

    remove auto fold

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -114,14 +114,6 @@ return {
             },
         },
         config = function(_, opts)
-            vim.opt.foldmethod = "expr"
-            vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-            vim.wo.foldlevel = 99
-            -- vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile", "BufWinEnter" }, {
-            --     pattern = { "*" },
-            --     callback = "normal zR"
-            -- })
-
             require("nvim-treesitter.configs").setup(opts)
         end,
         keys = {
