5fd70b66525e907333fe547115fb88cec3836005
Adrian Hornung <adrian.hornung@yahoo.de>
2024-05-26 09:28:09 +0200

    remove scala

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -115,45 +115,4 @@ return {
         version = "^2",
         ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
     },
-    -- Scala LSP
-    {
-        "scalameta/nvim-metals",
-        name = "metals",
-        ft = { "scala", "sbt", "java" },
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local metals_config = require("metals").bare_config()
-
-            -- Example of settings
-            metals_config.settings = {
-                showImplicitArguments = true,
-                excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-            }
-
-            -- *READ THIS*
-            -- I *highly* recommend setting statusBarProvider to true, however if you do,
-            -- you *have* to have a setting to display this in your statusline or else
-            -- you'll not see any messages from metals. There is more info in the help
-            -- docs about this
-            metals_config.settings = {
-                showImplicitArguments = true,
-                showImplicitConversionsAndClasses = true,
-                showInferredType = true,
-                superMethodLensesEnabled = true,
-            }
-            -- metals_config.init_options.statusBarProvider = "on"
-            metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            require("metals").initialize_or_attach(metals_config)
-        end,
-        keys = {
-            {
-                "<leader>mc",
-                function()
-                    require("telescope").extensions.metals.commands()
-                end,
-                desc = "Metals Commands",
-            }
-        },
-    },
 }
