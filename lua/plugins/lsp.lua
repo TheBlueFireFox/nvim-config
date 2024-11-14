fe6265e4451547319f8ac1142ac37bb7ade40aee
Hornung Adrian <adrian.hornung@yahoo.de>
2024-11-14 09:55:54 +0100

    workaround for rust_analyzer: -32802 sever cancelled the request

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -46,6 +46,18 @@ return {
         "mrcjkb/rustaceanvim",
         version = "^5", -- Recommended
         ft = { "rust" },
+        init = function()
+            -- Add workaround for https://github.com/neovim/neovim/issues/30985
+            for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+                local default_diagnostic_handler = vim.lsp.handlers[method]
+                vim.lsp.handlers[method] = function(err, result, context, config)
+                    if err ~= nil and err.code == -32802 then
+                        return
+                    end
+                    return default_diagnostic_handler(err, result, context, config)
+                end
+            end
+        end
     },
     {
         "saecki/crates.nvim",
