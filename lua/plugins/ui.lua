a23ae1c4fea5b735c933e84f8227607d79846cdf
Adrian Hornung <adrian.hornung@yahoo.de>
2025-01-03 15:19:18 -0500

    remove not needed TS code

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -117,12 +117,7 @@ return {
                 "vimdoc",
                 "yaml",
             },
-            highlight = {
-                enable = true,
-                disable = { "tex" },
-                additional_vim_regex_highlighting = false,
-            },
-        },
+         },
         config = function(_, opts)
             require("nvim-treesitter.configs").setup(opts)
         end,
