22910a61b2513bfe82852c5df5e1d3a7a5bca263
Hornung Adrian <adrian.hornung@yahoo.de>
2025-05-12 13:08:52 +0200

    update to mason 2.0

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,33 +4,18 @@ return {
         "mason-org/mason-lspconfig.nvim",
         dependencies = {
             { "neovim/nvim-lspconfig" },
-            { "mason-org/mason.nvim", version = "^1.0.0" },
+            { "mason-org/mason.nvim", opts = {} },
         },
-        event = { "VeryLazy" },
-        version = "^1.0.0",
-        opts = {},
-        config = function()
-            local lsp_config = require("lspconfig")
-            local mason_config = require("mason-lspconfig")
-            mason_config.setup({})
-
-            lsp_config.util.default_config = vim.tbl_extend("force", lsp_config.util.default_config, {
-                capabilities = require("cmp_nvim_lsp").default_capabilities(),
-            })
-            require("mason").setup({})
-
-            -- Register a handler that will be called for each installed server when it's ready (i.e.
-            -- when installation is finished or if the server is already installed).
-            mason_config.setup_handlers({
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
-                ["rust_analyzer"] = function()
-                end,
-                ["hls"] = function()
-                end
-            })
-        end,
+        lazy = false,
+        opts = {
+            ensure_installed = { "lua_ls", "vimls" },
+            automatic_enable = {
+                exclude = {
+                    "rust_analyzer",
+                    "hls"
+                }
+            }
+        },
     },
     -- lsp inlay hints
     {
@@ -39,24 +24,12 @@ return {
         dependencies = { "neovim/nvim-lspconfig" },
         config = function()
             require("inlay-hints").setup()
-        end
+        end,
     },
     -- rust analyzer super powers
     {
         "mrcjkb/rustaceanvim",
-        version = "^5", -- Recommended
+        version = "^6", -- Recommended
         ft = { "rust" },
-        init = function()
-            -- Add workaround for https://github.com/neovim/neovim/issues/30985
-            for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
-                local default_diagnostic_handler = vim.lsp.handlers[method]
-                vim.lsp.handlers[method] = function(err, result, context, config)
-                    if err ~= nil and err.code == -32802 then
-                        return
-                    end
-                    return default_diagnostic_handler(err, result, context, config)
-                end
-            end
-        end
     },
 }
