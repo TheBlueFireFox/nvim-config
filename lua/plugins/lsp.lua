e35fb18034220403d46cb4ef2550cb829ba577af
Hornung Adrian <adrian.hornung@yahoo.de>
2025-05-26 11:21:07 +0200

    add saecki/crates.nvim back in

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,4 +32,12 @@ return {
         version = "^6", -- Recommended
         ft = { "rust" },
     },
+    {
+        'saecki/crates.nvim',
+        tag = 'stable',
+        event = { "BufRead Cargo.toml" },
+        config = function()
+            require('crates').setup()
+        end,
+    }
 }
