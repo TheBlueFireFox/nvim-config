13a2e78d3e3731b3dc762f1dcb973fe6b77c969c
Adrian Hornung <adrian.hornung@yahoo.de>
2025-12-10 11:38:58 -0500

    Clean up dependency order

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -12,13 +12,15 @@ return {
             -- snippets
             {
                 "onsails/lspkind-nvim",
-                dependencies = { "saadparwaiz1/cmp_luasnip" }
             },
             {
                 "L3MON4D3/LuaSnip",
                 version = "v2.*",
                 build = "make install_jsregexp",
-                dependencies = { "rafamadriz/friendly-snippets" },
+                dependencies = {
+                    "rafamadriz/friendly-snippets",
+                    "saadparwaiz1/cmp_luasnip" -- luasnip
+                },
                 opts = function()
                     require("luasnip.loaders.from_vscode").lazy_load()
                 end,
